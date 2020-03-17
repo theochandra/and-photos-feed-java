4e6e22d0ff6e40cb3cb03c972369fe3cd4ff3c23
theodorous beatayuvens <theochandra27@gmail.com>
2020-03-17 11:19:42 +0700

    refactor main activity

--- app/src/main/java/com/android/photofeed/presentation/photolist/PhotoListActivity.java
+++ app/src/main/java/com/android/photofeed/presentation/photolist/PhotoListActivity.java
@@ -1,14 +1,16 @@
-package com.android.photofeed;
+package com.android.photofeed.presentation.photolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.android.photofeed.R;
+
+public class PhotoListActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_photo_list);
     }
 }
